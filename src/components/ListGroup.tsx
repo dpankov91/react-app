5111278bf4947fbc69fd61329b0528311768cf35
Dmitri Pankov <dmitri.pankov@rik.ee>
2025-01-31 20:04:57 +0200

    dynamic data in html

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,13 +1,18 @@
 function ListGroup() {
+  let items = ["London", "Tallinn", "Singapore"];
+  items = [];
+  const getMessage = () => {
+    return items.length === 0 && <p>No item found</p>;
+  };
+
   return (
     <>
       <h1>Holla</h1>
+      {getMessage()}
       <ul className="list-group">
-        <li className="list-group-item">An item</li>
-        <li className="list-group-item">A second item</li>
-        <li className="list-group-item">A third item</li>
-        <li className="list-group-item">A fourth item</li>
-        <li className="list-group-item">And a fifth one</li>
+        {items.map((item) => (
+          <li key={item}>{item}</li>
+        ))}
       </ul>
     </>
   );
