b78733183ec3dcfa0e013413487c5ea4d1488249
Dmitri Pankov <dmitri.pankov@rik.ee>
2025-02-01 11:00:00 +0200

    props added

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,19 @@
 import ListGroup from "./components/ListGroup";
 
 function App() {
-  return <div><ListGroup /></div>
+  let items = ["London", "Tallinn", "Singapore"];
+
+  const handleSelectItem = (item: string) => console.log(item);
+
+  return (
+    <div>
+      <ListGroup
+        items={items}
+        heading="Cities"
+        onSelectItem={handleSelectItem}
+      />
+    </div>
+  );
 }
 
 export default App;
