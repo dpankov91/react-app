a521e3f9eb683c7fd286e573ff6e1d9f0a03eb80
Dmitri Pankov <dmitri.pankov@rik.ee>
2025-02-02 12:26:12 +0200

    binding alert and button onclick

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,16 @@
+import { useState } from "react";
 import Alert from "./components/Alert";
 import Button from "./components/Button";
 
 function App() {
+  const [alertVisible, setAlertVisiblility] = useState(false);
+
   return (
     <div>
-      <Button color="danger" onClick={() => console.log("Clicked")}>
+      {alertVisible && (
+        <Alert onClose={() => setAlertVisiblility(false)}> Open Alert </Alert>
+      )}
+      <Button color="primary" onClick={() => setAlertVisiblility(true)}>
         My Button
       </Button>
     </div>
