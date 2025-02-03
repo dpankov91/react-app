dd9f91ac4434f724a4f48c656b6bb7740086f67d
Dmitri Pankov <dmitri.pankov@rik.ee>
2025-02-03 20:57:16 +0200

    css added

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,35 @@
 import { useState } from "react";
 import Alert from "./components/Alert";
 import Button from "./components/Button";
+import ListGroup from "./components/ListGroup/ListGroup";
 
 function App() {
-  const [alertVisible, setAlertVisiblility] = useState(false);
+  const cities = ["Tallinn", "Berlin", "Barcelona"];
+  const handleSelectItem = (item: string) => {
+    console.log(item);
+  };
 
   return (
     <div>
-      {alertVisible && (
-        <Alert onClose={() => setAlertVisiblility(false)}> Open Alert </Alert>
-      )}
-      <Button color="primary" onClick={() => setAlertVisiblility(true)}>
-        My Button
-      </Button>
+      <ListGroup
+        items={cities}
+        heading="Cities"
+        onSelectItem={handleSelectItem}
+      ></ListGroup>
     </div>
   );
+  //   const [alertVisible, setAlertVisiblility] = useState(false);
+
+  //   return (
+  //     <div>
+  //       {alertVisible && (
+  //         <Alert onClose={() => setAlertVisiblility(false)}> Open Alert </Alert>
+  //       )}
+  //       <Button color="primary" onClick={() => setAlertVisiblility(true)}>
+  //         My Button
+  //       </Button>
+  //     </div>
+  //   );
 }
 
 export default App;
