7ba452c679154fc594024e635811c11752c40a99
Dmitri Pankov <dmitri.pankov@rik.ee>
2025-03-19 16:59:24 +0200

    form with react-hook-form

---
+++ src/components/Form.tsx
@@ -0,0 +1,40 @@
+import React, { FormEvent, useState } from "react";
+import { FieldValues, useForm } from "react-hook-form";
+
+const Form = () => {
+  const { register, handleSubmit } = useForm();
+
+  const onSubmit = (data: FieldValues) => console.log(data);
+
+  return (
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <div className="mb-3">
+        <label htmlFor="name" className="form-label">
+          Name
+        </label>
+        <input
+          id="name"
+          type="text"
+          {...register("name")}
+          className="form-control"
+        />
+      </div>
+      <div className="mb-3">
+        <label htmlFor="age" className="form-label">
+          Age
+        </label>
+        <input
+          id="age"
+          type="number"
+          {...register("age")}
+          className="form-control"
+        />
+      </div>
+      <button className="btn btn-primary" type="submit">
+        Submit
+      </button>
+    </form>
+  );
+};
+
+export default Form;
